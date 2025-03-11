9411972153238d0991bed2d50725f3b2ff1a223d
Mark de Leon <mark_deleon@hotmail.com>
2025-03-11 19:11:47 +0800

    Initial commit

---
+++ components/NavBar.vue
@@ -0,0 +1,50 @@
+<template>
+    <div id="nav">
+      <NuxtLink to="/">Home</NuxtLink> |
+    </div>
+  </template>
+  
+  <script>
+  export default {}
+  </script>
+  
+  <style scoped>
+  #nav {
+    padding: 30px;
+  }
+  
+  #nav a {
+    font-weight: bold;
+    color: #2c3e50;
+    text-decoration: underline;
+  }
+  
+  #nav a:hover {
+    background-color: lightblue;
+  }
+  
+  #nav a.nuxt-link-exact-active {
+    color: royalblue;
+    text-decoration: none;
+    background-color: white;
+  }
+  
+  @media (max-width: 359px) {
+    #nav {
+      display: grid;
+    }
+  }
+  @media (min-width: 360px) {
+    #nav {
+      padding-left: 80px;
+      padding-right: 80px;
+    }
+  }
+  @media (min-width: 411px) {
+    #nav {
+      padding-left: 50px;
+      padding-right: 50px;
+    }
+  }
+  </style>
+  
---
+++ layouts/default.vue
@@ -0,0 +1,46 @@
+<template>
+    <div id="app">
+        <nav>
+            <NavBar />
+        </nav>
+        <main>
+            <div id="page-header">
+                <h1>U P P E R C A S E</h1>
+            </div>
+            <div id="page-section">
+                <slot />
+            </div>
+        </main>
+    </div>
+</template>
+
+<script>
+import NavBar from '@/components/NavBar.vue'
+
+export default {
+  components: {
+    NavBar
+  }
+}
+</script>
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  display: grid;
+}
+
+#page-header {
+  border-bottom: 1px;
+  border-bottom-color: grey;
+  border-bottom-style: solid;
+}
+
+#page-section {
+  padding-top: 20px;
+}
+
+</style>
