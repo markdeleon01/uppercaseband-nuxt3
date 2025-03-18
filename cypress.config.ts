b41214a40cc906af41502a4813364b67eda264c2
Mark de Leon <mark_deleon@hotmail.com>
2025-03-18 15:13:33 +0800

    Cypress recording

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from "cypress";
 
 export default defineConfig({
+  projectId: "n96fcn",
   e2e: {
     specPattern: 'cypress/e2e/**/*.{cy,spec}.{js,jsx,ts,tsx}',
     baseUrl: 'http://localhost:3000',
-    
+
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
